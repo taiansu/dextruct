b3f56cb78451935e079267b7032090db143490e9
Tai An Su <taiansu@gmail.com>
2017-08-18 16:43:54 +0800

    Let's start from here

---
+++ lib/dextruct.ex
@@ -0,0 +1,89 @@
+defmodule Dextruct do
+  @moduledoc """
+  Documentation for Dextruct.
+  """
+
+  @doc """
+  """
+  defmacro __using__(opts \\ []) do
+    quote do
+      import Dextruct
+
+      defmacro left <~ right when is_list(right) do
+        len = length(left)
+        elem = unquote(opts[:with])
+        list = fill(right, len, elem)
+
+        quote do
+          unquote(left) = unquote(list)
+        end
+      end
+
+      defmacro left <~ right do
+        elem = unquote(opts[:with])
+        keys = left
+               |> Macro.expand_once(__ENV__)
+               |> fetch_keys
+
+        quote do
+          unquote(left) = fill(unquote(right), unquote(keys), unquote(elem))
+        end
+      end
+    end
+  end
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dextruct.fill([1], 3)
+      [1, nil, nil]
+
+  ## Example
+
+      iex> Dextruct.fill(%{a: 1}, [:a, :b, :c])
+      %{a: 1, b: nil, c: nil}
+
+  """
+  @spec fill(Enum.t, integer | list, any) :: Enum.t
+  def fill(enum, length_or_keys, elem \\ nil)
+  def fill(list, length, elem) when is_list(list) do
+    filler = List.duplicate(elem, length)
+
+    list
+    |> :erlang.++(filler)
+    |> Enum.take(length)
+  end
+  def fill(map, keys, elem) when is_map(map) do
+    filler = Map.new(keys, fn k -> {k, elem} end)
+
+    Map.merge(filler, map)
+  end
+
+  @doc """
+  """
+  defmacro sigil_m({:<<>>, _line, [string]}, opt) do
+    ast = string
+          |> String.split(~r/,(?:\s)*/)
+          |> Enum.map(&String.split(&1, ~r/(:\s|\s=>\s)/))
+          |> Enum.map(&to_variable_ast(&1, opt))
+
+    {:%{}, [], ast}
+  end
+
+  defp to_variable_ast([key], _opt) do
+    k = String.to_atom(key)
+    # unbinding variable is unhygienic
+    {k, {k, [], nil}}
+  end
+
+  defp to_variable_ast([key, val], _opt) do
+    {String.to_atom(key), {String.to_atom(val), [], nil}}
+  end
+
+  def fetch_keys({:%{}, _, args}) do
+    Keyword.keys(args)
+  end
+  def fetch_keys(_ast), do: raise SyntaxError
+end
---
+++ test/dextruct_test.exs
@@ -0,0 +1,40 @@
+defmodule DextructTest do
+  use ExUnit.Case
+
+  doctest Dextruct, except: [sigil_m: 2]
+  use Dextruct, with: 0
+
+  test "Destruct with list" do
+    # [a, b] <~ Regex.run(~r/(\d+)?([A-Z]+)?/, "1", capture: :all_but_first)
+    # assert [a, b] == ["1", nil]
+  end
+
+  test "Destruct with map" do
+    %{a: a, b: b, c: foo} <~ %{a: 1}
+    assert a == 1
+    assert b == 0
+    assert foo == 0
+  end
+
+  test "Destruct with sigil_m" do
+    ~m{a, b, c: foo} <~ %{a: 1}
+    assert a == 1
+    assert b == 0
+    assert foo == 0
+  end
+
+  test "fill" do
+  end
+
+  test "~m{} expands keys to map of atom" do
+    ~m{a, b, c: foo} = %{a: 1, b: 2, c: 3}
+    assert a == 1
+    assert b == 2
+    assert foo == 3
+  end
+
+  test "~m with string" do
+    ~m{a, b, c => foo} = %{a: 1, b: 2, c: 3}
+  end
+
+end
