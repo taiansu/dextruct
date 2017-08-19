200187e53f89d7831ac207d529d183a7bba21cc8
Tai An Su <taiansu@gmail.com>
2017-08-19 18:28:16 +0800

    Adjust mix file, add mix.lock

--- mix.exs
+++ mix.exs
@@ -4,22 +4,43 @@ defmodule Dextruct.Mixfile do
   def project do
     [
       app: :dextruct,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      name: "Dextruct",
+      source_url: "https://github.com/taiansu/dextruct",
+      homepage_url: "https://taiansu.github.io/dextruct",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
+    [extra_applications: [:logger]]
+  end
+
+  def description do
+    """
+    Destructing assignment with `<~`
+    """
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      files: ~s/lib priv mix.exs README.md LICENSE/,
+      maintainers: ["Tai An Su"],
+      licenses: ["Apache 2.0"],
+      links: %{
+               "GitHub": "https://github.com/taiansu/dextruct",
+               "Docs": "https://taiansu.github.io/dextruct",
+              },
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.16", only: :dev},
+      {:excoveralls, "~> 0.7", only: [:dev, :test]},
+    ]
   end
 end
